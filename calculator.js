16596a3b031d59400228b6ac029122585dcf0c46
Sydney Brown <bsydney95@gmail.com>
2021-11-13 08:16:05 -0500

    Begun to add the logic of numbers and their operators.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const btns = document.querySelectorAll("button");
+const display = document.querySelector("output");
+let firstNum;
+let nextNum;
+let operator;
+let equation = [firstNum, nextNum];
+
 const add = function(num){
     let result = num.reduce((total, current) => {return total + current});
     return result;
@@ -16,13 +25,41 @@ const divide = function(num){
 };
 
 function operate(operator, num1, num2) {
-    
+
 };
 
-const btns = document.querySelectorAll("button");
-const out = document.querySelector("output");
+//Makes the button value appear on the display area.
 btns.forEach((button) => {
     button.addEventListener("click", (e) => {
-        out.textContent = button.textContent;
+        display.textContent = button.textContent;
+        if (button.className=="number")
+        {
+            if (firstNum == (null || undefined))
+            {
+                firstNum = Number(button.textContent);
+            }
+            else 
+            {
+                if (nextNum != (null || undefined))
+                {
+                    firstNum = firstNum + nextNum;
+                    nextNum = Number(button.textContent);
+                }
+                else {
+                    nextNum = Number(button.textContent);
+                }
+            }
+            console.log(firstNum, nextNum);
+        }
+        if (button.className=="operator")
+        {
+            if (firstNum == (null || undefined))
+            {
+                display.textContent = "Please enter a number."
+            }
+            else {
+                
+            }
+        }
     })
 })
