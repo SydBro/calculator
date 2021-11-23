8cd82c11de8f36e1fd4de3f959840d84897d724d
Sydney Brown <bsydney95@gmail.com>
2021-11-22 20:59:24 -0500

    Calculations work, it's just a matter of storing numbers into variables and executing the functions in the right order, so that it all displays properly. But the Operate() and each respective operator are all working functions, so I'm happy.

--- calculator.js
+++ calculator.js
@@ -2,48 +2,37 @@ const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 const btns = document.querySelectorAll("button");
 const display = document.querySelector("output");
+const eqls = document.querySelector("#equals");
 let firstNum;
 let nextNum;
 let operator;
-let equation = [firstNum, nextNum];
-
-// const add = function(num){
-//     let result = num.reduce((total, current) => {return total + current});
-//     return result;
-// };
-// const subtract = function(num){
-//     let result = num.reduce((total, current) => {return total - current});
-//     return result;
-// };
-// const multiply = function(num){
-//     let result = num.reduce((total, current) => {return total * current});
-//     return result;
-// };
-// const divide = function(num){
-//     let result = num.reduce((total, current) => {return total / current});
-//     return result;
-// };
+let equation;
 
 const add = function(num1, num2){
-    additive = num1 + num2;
-    console.log(additive);
     return num1 + num2;
 };
 const subtract = function(num1, num2){
-    return result;
+    return num1 - num2;
 };
 const multiply = function(num1, num2){
-    return result;
+    return num1 * num2;
 };
 const divide = function(num1, num2){
-    return result;
+    return (num1 / num2);
 };
 
 function operate(operator, num1, num2) {
     switch(operator){
         case "+":
-            add(num1, num2);
-            break;
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "&divide":
+            return divide(num1, num2);
+        default:
+            return "Nothing happened.";
     }
 
 };
@@ -51,107 +40,58 @@ function operate(operator, num1, num2) {
 //Makes the button value appear on the display area.
 btns.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if (button.className == "number") {
+        
+        //display a number
+        //when an operator is hit, store the number in variable firstNum & clear the box
+        //when an equal sign is hit, store the number in variable nextNum & show the answer
+
+        if (button.className == "number")
+        {
             display.textContent += button.textContent;
         }
-        if (button.className == "operator") {
-            if (button.id != "equals") {
-                operator = button.textContent;
+
+        if (button.className == "operator")
+        {
+            // if ((operator && firstNum && nextNum) != ("" || null))
+            // {
+            //     firstNum = operate(operator, firstNum, nextNum);
+            //     nextNum = Number(display.textContent);
+            //     operator = button.textContent;
+            //     display.textContent = "";
+            //     console.log(operator, firstNum, nextNum)
+            // }
+            if ((display.textContent == (null || "") && firstNum == (null || "")))
+            {
+                firstNum = 0;
             }
-            else {
-                operate(operator, firstNum, nextNum);
-            }
-            
-            if (firstNum == null || undefined) {
-                firstNum = Number(display.textContent);
-                display.textContent = ""
-            }
-            else {
+            if ((operator && firstNum) != ("" || null))
+            {
                 nextNum = Number(display.textContent);
-                display.textContent = ""
+                firstNum = operate(operator, firstNum, nextNum);
+                operator = button.textContent;
+                display.textContent = "";
+                console.log(operator, firstNum, nextNum)
+            }
+            else {
+                firstNum = Number(display.textContent);
+                operator = button.textContent;
+                display.textContent = "";
+                console.log(operator, firstNum, nextNum)
             }
         }
-        console.log(firstNum, operator, nextNum)
+        console.log(display.textContent)
+
     })
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// numbers.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         display.textContent += button.textContent;
-//         if (button.className=="number")
-//         {
-//             if (firstNum == (null || undefined))
-//             {
-//                 firstNum = Number(button.textContent);
-//             }
-//             else 
-//             {
-//                 if (nextNum != (null || undefined))
-//                 {
-//                     firstNum = firstNum + nextNum;
-//                     nextNum = Number(button.textContent);
-//                 }
-//                 else {
-//                     nextNum = Number(button.textContent);
-//                 }
-//             }
-//             console.log(firstNum, nextNum);
-//         }
-//     })
-// })
-
-// operators.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         if (firstNum == (null || undefined))
-//         {
-//             display.textContent = "Please enter a number."
-//         }
-//         else {
-//             firstNum = Number(display.textContent);
-//         }
-//     })
-// })
+eqls.addEventListener("click", (e) => {
+    if (nextNum == null)
+    {
+        nextNum = Number(display.textContent);
+    }
+    display.textContent = operate(operator, firstNum, nextNum);
+    console.log(operator, firstNum, nextNum)
+    firstNum = "";
+    nextNum = "";
+    operator = "";
+})
