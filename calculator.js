2471c2601e4060611ac48cde07455316c66d9ed3
Sydney B <sydglyph@gmail.com>
2021-12-01 13:17:37 -0500

    Got the calculations working. Claculator works like it should.

--- calculator.js
+++ calculator.js
@@ -30,82 +30,64 @@ function operate(operator, num1, num2) {
             return subtract(num1, num2);
         case "x":
             return multiply(num1, num2);
-        case "&divide":
+        case "÷":
             return divide(num1, num2);
         default:
             return "Nothing happened.";
     }
-
 };
 
 //Makes the button value appear on the display area.
-btns.forEach((button) => {
+numbers.forEach((button) => {
     button.addEventListener("click", (e) => {
-        
-        //display a number
-        //when an operator is hit, store the number in variable firstNum & clear the box
-        //when an equal sign is hit, store the number in variable nextNum & show the answer
+        if (button.textContent == "." && display.textContent.includes(".")) return;
+        display.textContent += button.textContent;
+    })
+})
 
-        if (button.className == "number")
+operators.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if ((operator && firstNum) != ("" || null))
         {
-            if (display.textContent == (firstNum || nextNum))
-            {
-                display.textContent = "";
-            }
-            display.textContent += button.textContent;
+            nextNum = Number(display.textContent);
+            display.textContent = "";
+            firstNum = operate(operator, firstNum, nextNum);
+            operator = button.textContent;
+            nextNum = "";
+            console.log(operator, firstNum, nextNum)
         }
-
-        if (button.className == "operator")
-        {
-            if ((display.textContent == (null || "") && firstNum == (null || "")))
-            {
-                firstNum = 0;
-            }
-            if ((operator && firstNum && nextNum) != ("" || null))
-            {
-                nextNum = Number(display.textContent);
-                operator = button.textContent;
-                display.textContent = "";
-                console.log(operator, firstNum, nextNum)
-            }
-            if ((operator && firstNum) != ("" || null))
-            {
-                nextNum = Number(display.textContent);
-                firstNum = operate(operator, firstNum, nextNum);
-                operator = button.textContent;
-                display.textContent = firstNum;
-                console.log(operator, firstNum, nextNum)
-            }
-            else {
-                firstNum = Number(display.textContent);
-                operator = button.textContent;
-                display.textContent = "";
-                console.log(operator, firstNum, nextNum)
-            }
+        else {
+            firstNum = Number(display.textContent);
+            operator = button.textContent;
+            display.textContent = "";
+            console.log(operator, firstNum, nextNum)
         }
-
     })
 })
 
 eqls.addEventListener("click", (e) => {
-    if (nextNum == null)
+    if (firstNum != null && nextNum == "")
     {
         nextNum = Number(display.textContent);
     }
     display.textContent = operate(operator, firstNum, nextNum);
     console.log(operator, firstNum, nextNum)
-    firstNum = null;
-    nextNum = null;
-    operator = null;
+    firstNum = "";
+    nextNum = "";
+    operator = undefined;
 })
 
+//done
+//clears every value and display
 clear.addEventListener("click", (e) => {
-    firstNum = null;
-    nextNum = null;
-    operator = null;
+    firstNum = "";
+    nextNum = "";
+    operator = undefined;
     display.textContent = "";
 })
 
+//done
+//backspaces a number
 back.addEventListener("click", (e) => {
     lastLetterGone = display.textContent.slice(0,-1);
     display.textContent = lastLetterGone;
